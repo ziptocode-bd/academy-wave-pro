f549e1182a86de6d599abfd7212dfe49ad3875d3
ziptocode-bd <ziptocode.bd@gmail.com>
2026-05-09 00:32:49 +0600

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { AppSettingsProvider } from "@/contexts/AppSettingsContext";
@@ -28,9 +30,13 @@ import ExamTakePage from "@/pages/ExamTakePage";
 import AdminDataPage from "@/pages/admin/AdminDataPage";
 import NotFound from "@/pages/NotFound";
 
+const queryClient = new QueryClient();
+
 const App = () => (
-  <TooltipProvider>
-    <Sonner />
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
       <BrowserRouter>
         <AuthProvider>
           <AppSettingsProvider>
@@ -65,7 +71,8 @@ const App = () => (
           </AppSettingsProvider>
         </AuthProvider>
       </BrowserRouter>
-  </TooltipProvider>
+    </TooltipProvider>
+  </QueryClientProvider>
 );
 
 export default App;
